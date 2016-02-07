6df2a6ad89041c84a44ac41d498d941bef525251
Rekseto <xsavine@gmail.com>
2016-02-07 10:41:34 +0100

    - First Unit - New Objects MapObject , TerrainObject  new Enum TerrainObjectType

--- Your Empire API/src/main/java/pl/yourempire/api/unit/Unit.java
+++ Your Empire API/src/main/java/pl/yourempire/api/unit/Unit.java
@@ -1,8 +1,68 @@
 package pl.yourempire.api.unit;
 
+import pl.yourempire.api.object.TerrainObject;
+import pl.yourempire.api.object.TerrainObjectType;
 import pl.yourempire.api.unit.info.UnitInfo;
 
-public interface Unit extends UnitInfo
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administratorek on 2016-02-07.
+ */
+public class Unit implements UnitInfo
 {
-    
+
+    private int HP;
+    private int DMG;
+    private int SPD;
+    private List<TerrainObjectType> TerrainCanMoves = new ArrayList<TerrainObjectType>();
+    private List<TerrainObjectType> BonusTerrain = new ArrayList<TerrainObjectType>();
+
+
+    public int getHP()
+    {
+        return this.HP;
+    }
+
+
+    public int getDMG()
+    {
+        return this.DMG;
+    }
+
+
+    public int getSPD()
+    {
+        return this.SPD;
+    }
+
+    public int getATKBonus()
+    {
+        return this.DMG;
+    }
+
+    public void setHP(int hp)
+    {
+        this.HP = hp;
+    }
+    public void setDMG(int dmg)
+    {
+        this.DMG = dmg;
+    }
+    public void setSPD(int spd)
+    {
+        this.SPD = spd;
+    }
+
+    public void addTerrainCanMove(TerrainObjectType t)
+    {
+        this.TerrainCanMoves.add(t);
+    }
+
+    public void addBonusTerrain(TerrainObjectType t)
+    {
+        this.BonusTerrain.add(t);
+    }
+
 }
