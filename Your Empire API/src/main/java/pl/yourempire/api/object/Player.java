9032527d23a8718a948fe131c1f6c7d7a32b8354
mikigal <mikigal.priv@gmail.com>
2016-03-01 16:42:22 +0100

    Fixed CityObject for new Religion object

--- Your Empire API/src/main/java/pl/yourempire/api/object/Player.java
+++ Your Empire API/src/main/java/pl/yourempire/api/object/Player.java
@@ -1,13 +1,24 @@
 package pl.yourempire.api.object;
 
+import java.util.UUID;
+
 /**
  * Created by mikigal on 2016-02-29.
  */
 public class Player {
     private ID id;
+    private UUID uuid;
     private Integer money;
     private CityObject[] cities;
-    private Bonus bonus;
+    private Commander commander;
+
+    public Player(ID id, UUID uuid, Integer money, CityObject[] cities, Commander commander) {
+        this.id = id;
+        this.uuid = uuid;
+        this.money = money;
+        this.cities = cities;
+        this.commander = commander;
+    }
 
     public ID getId() {
         return this.id;
@@ -33,11 +44,23 @@ public class Player {
         this.cities = cities;
     }
 
-    public Bonus getBonus() {
-        return this.bonus;
+    public UUID getUuid() {
+        return uuid;
     }
 
-    public void setBonus(Bonus bonus) {
-        this.bonus = bonus;
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Commander getCommander() {
+        return commander;
+    }
+
+    public void setCommander(Commander commander) {
+        this.commander = commander;
+    }
+
+    public void save(){
+        //TODO: Save Player object to file
     }
 }
