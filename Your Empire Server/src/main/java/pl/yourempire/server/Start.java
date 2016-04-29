07128cba35480a9b4b0064e6acecb1c1728d7d36
mikigal <mikigal.priv@gmail.com>
2016-04-29 14:27:20 +0200

    Little Server updae

--- Your Empire Server/src/main/java/pl/yourempire/server/Start.java
+++ Your Empire Server/src/main/java/pl/yourempire/server/Start.java
@@ -1,5 +1,17 @@
 package pl.yourempire.server;
 
+import java.util.logging.Logger;
+
 public class Start
 {
+    public static final String VERSION = "0.1";
+    static final Logger log = Logger.getLogger("Your Empire");
+
+    public static void main(String args[]){
+        log.info("Staring Your Empire server " + VERSION + " Issues? Report them on GitHub - http://github.com/YourEmpire/YourEmpire/issues");
+    }
+
+    public static Logger getLogger(){
+        return log;
+    }
 }
--- Your Empire Server/src/main/java/pl/yourempire/server/server/Server.java
+++ Your Empire Server/src/main/java/pl/yourempire/server/server/Server.java
@@ -1,5 +1,39 @@
 package pl.yourempire.server.server;
 
+import pl.yourempire.api.game.Game;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Server
 {
+    private Game game;
+    private InetAddress ip;
+    private int port;
+
+    public Server(Game game, int port) throws UnknownHostException {
+        this.game = game;
+        this.ip = InetAddress.getLocalHost();
+        this.port = port;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public InetAddress getIp() {
+        return ip;
+    }
 }
